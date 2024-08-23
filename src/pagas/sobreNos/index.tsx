323c53aa2a71451f8a0aca5400a1c7172888ca4a
Maria Helena Carvalho <96885338+MariaCHelena@users.noreply.github.com>
2024-08-22 22:35:39 -0300

    config: configurando caminho global dos arquivos e refatorando estrutura de pastas

--- src/pagas/sobreNos/index.tsx
+++
@@ -1,5 +0,0 @@
-export default function SobreNos(){
-    return<main>
-        <h1>OI</h1>
-    </main>
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/BullyingMap-Site"
+  base: "/BullyingMap-Site",
+  server: {
+    port: 3000
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  },
 })
