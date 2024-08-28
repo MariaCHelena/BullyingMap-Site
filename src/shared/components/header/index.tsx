9bc4a2a6ea81b3bfd2177273b467d93bd166ce6d
m4rllon <marllonpc33@gmail.com>
2024-08-28 14:24:24 -0300

    feat: Adicionar página Edital

--- src/shared/components/header/index.tsx
+++ src/shared/components/header/index.tsx
@@ -5,7 +5,7 @@ export default function Header(){
     return<header className="flex gap-5">
         <Link to={'/BullyingMap-Site'}>O PROJETO</Link>
         <Link to={'/BullyingMap-Site/nossaequipe'}>NOSSA EQUIPE</Link>
-        <Link to={'*'}></Link>
+        <Link to={'/BullyingMap-Site/edital/cotacao-publica-precos-No-001/2024'}>EDITAL</Link>
         <Link to={'*'}></Link>
         <Link to={'*'}></Link>
     </header>
