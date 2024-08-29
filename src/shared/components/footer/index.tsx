4e4f77abee239ae5c62631771bad95046eab727f
Maria Helena Carvalho <96885338+MariaCHelena@users.noreply.github.com>
2024-08-28 23:15:18 -0300

    feat: criando componentes e estilização do footer

--- src/shared/components/footer/index.tsx
+++ src/shared/components/footer/index.tsx
@@ -1,5 +1,25 @@
+import logo from "@/assets/Bullying Map_Wireframe_Sobre_v5-03.png"
+
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="w-full p-14 flex justify-between items-center bg-azulClaro mt-24 flex-col xl:flex-row gap-10">
+      <div className="hidden flex-col xl:flex flex-1">
+        <span className="font-bold text-white">EMAIL</span>
+        <span className="text-white">BULLYINGMAP.FAPDF@GMAIL.COM</span>
+      </div>
+      <div className="flex justify-center gap-6 flex-1">
+        <div className="w-20 h-20 -mt-[136px] bg-azulClaro"></div>
+        <img src={logo} className="h-40"/>
+        <div className="w-20 h-20 bg-fundo -mt-14"></div>
+      </div>
+      <div className="flex flex-col xl:hidden items-center">
+        <span className="font-bold text-white">EMAIL</span>
+        <span className="text-white">BULLYINGMAP.FAPDF@GMAIL.COM</span>
+      </div>
+      <div className="flex flex-col flex-1 xl:items-end items-center">
+        <span className="font-bold text-white">INSTAGRAM</span>
+        <span className="text-white">@BULLYINGMAP</span>
+      </div>
+    </footer>
   )
 }
