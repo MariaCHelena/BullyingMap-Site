1947bab1d378d6a583e0f7227e5ff2ae9cbb67d8
m4rllon <marllonpc33@gmail.com>
2024-09-02 09:06:44 -0300

    style: ajustando responsividade da página Nossa Equipe

--- src/features/edital/pages/index.tsx
+++ src/features/edital/pages/index.tsx
@@ -1,14 +1,14 @@
 import arvore from '../images/arvore.png'
 
 const EditalPage = () => {
-    return<div className=" flex flex-col items-center h-auto gap-16 md:mx-36 mt-5">
+    return<div className="md:relative flex flex-col items-center h-auto gap-16 md:mx-36 mt-5">
         <div className='flex flex-col justify-start gap-3 px-16'>
             <h1 className=" text-laranja text-4xl font-bold">EDITAL</h1>
             <h2 className="text-azul text-lg font-bold">COTAÇÃO PÚBLICA DE PREÇOS NO 001/2024</h2>
             <p className="text-preto font-medium text-justify">Projeto Bullyingmap 2.0 – Gamificação Ativa para o Enfrentamento à Intimidação Sistemática (Bullying) Nas Escolas (Chamada 04/2023 Tech Learning – Vinculada ao Edital 10/2023 – Programa FAPDF Learning da Fundação de Apoio à Pesquisa do Distrito Federal – FAP-DF).</p>
             <p className="text-preto font-medium">Para ler o edital completo, <a className="underline" href="https://bullyingmap.com/wp-content/uploads/2024/08/Edital-Cotacao-precos-bullying-map-final.pdf" target="__blank">acesse</a>.</p>
         </div>
-        <img className='top-0 right-0  w-2/3 sm:w-2/3 md:w-3/5 lg:w-2/5' src={arvore} alt="Arvore" />
+        <img className='md:absolute top-28 -right-28  w-2/3 sm:w-2/3 md:w-56' src={arvore} alt="Arvore" />
     </div>
 }
 
--- src/features/nossa-equipe/pages/index.tsx
+++ src/features/nossa-equipe/pages/index.tsx
@@ -6,7 +6,7 @@ export default function NossaEquipe(){
         <div>
             <div className='relative mx-4'>
                 <img className='absolute top-center right-0 w-1/5 h-w-1/5 xl:w-56 xl:h-56 hidden md:block' src={sol} alt="Sol" />
-                <h1 className='flex items-center justify-center text-laranja text-4xl h-10 md:h-56 font-bold'>NOSSA EQUIPE</h1>
+                <h1 className='flex items-center justify-center text-laranja text-4xl h-10 md:h-56 font-bold my-10'>NOSSA EQUIPE</h1>
             </div>
             <div className='flex flex-col justify-center items-center'>
                 <Carrossel/>
